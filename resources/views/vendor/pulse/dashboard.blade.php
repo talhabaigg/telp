2636ae88760478c4f584f480471afa72dceb055e
hsbmaulana <hsbmaulana.iso@protonmail.com>
2024-10-10 17:21:02 +0700

    feat(config): log

---
+++ resources/views/vendor/pulse/dashboard.blade.php
@@ -0,0 +1,22 @@
+<x-pulse>
+
+    <livewire:pulse.servers cols="full" />
+
+    <livewire:pulse.usage cols="4" rows="2" />
+
+    <livewire:pulse.cache cols="4" />
+    <livewire:pulse.queues cols="4" />
+    <livewire:reverb.connections cols="4" />
+    <livewire:reverb.messages cols="4" />
+
+    <livewire:pulse.exceptions cols="full" />
+
+    <livewire:pulse.slow-queries cols="full" />
+
+    <livewire:pulse.slow-jobs cols="full" />
+
+    <livewire:pulse.slow-requests cols="6" />
+
+    <livewire:pulse.slow-outgoing-requests cols="6" />
+
+</x-pulse>
