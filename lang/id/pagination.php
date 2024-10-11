83def447544ed537174147380126cc47ba894d2c
hsbmaulana <hsbmaulana.iso@protonmail.com>
2024-10-12 06:30:37 +0700

    feat(config): i18n

---
+++ lang/id/pagination.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris Bahasa Paginasi
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut digunakan oleh pustaka paginator untuk membangun
+    | tautan paginasi sederhana. Anda bebas untuk mengubahnya sesuai
+    | keinginan Anda agar tampilan Anda lebih cocok dengan aplikasi Anda.
+    |
+    */
+
+    'previous' => '&laquo; Sebelumnya',
+    'next' => 'Selanjutnya &raquo;',
+
+];
---
+++ lang/id/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris Bahasa Reset Kata Sandi
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut adalah baris default yang cocok dengan alasan
+    | yang diberikan oleh broker kata sandi untuk hasil percobaan
+    | pembaruan kata sandi, seperti kegagalan akibat kata sandi atau token reset yang tidak valid.
+    |
+    */
+
+    'reset' => 'Kata sandi Anda telah direset.',
+    'sent' => 'Kami telah mengirimkan tautan reset kata sandi ke email Anda.',
+    'throttled' => 'Silakan tunggu sebelum mencoba lagi.',
+    'token' => 'Token reset kata sandi ini tidak valid.',
+    'user' => "Kami tidak dapat menemukan pengguna dengan alamat email tersebut.",
+
+];
