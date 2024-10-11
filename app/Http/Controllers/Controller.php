536607cae71493ea7f08acb023b6adec65b68f44
hsbmaulana <hsbmaulana.iso@protonmail.com>
2024-10-12 06:32:01 +0700

    feat(config): swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,19 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *      title="Application",
+ *      version="1.0"
+ * ),
+ * @OA\SecurityScheme(
+ *      securityScheme="bearerAuth",
+ *      in="header",
+ *      type="http",
+ *      scheme="bearer",
+ *      bearerFormat="JWT"
+ * )
+ */
 abstract class Controller
 {
     //
