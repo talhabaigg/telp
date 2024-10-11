c2cecf96b43cdfeb054cae39d143dac029b115f1
hsbmaulana <hsbmaulana.iso@protonmail.com>
2024-10-12 06:36:52 +0700

    feat: feature adminer setting profile

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 require __DIR__."/../src/V0/Setting/Routes/user/setting.php";
 require __DIR__."/../src/V0/Setting/Routes/admin/setting.php";
+require __DIR__."/../src/V0/SettingProfile/Routes/user/setting/profile.php";
+require __DIR__."/../src/V0/SettingProfile/Routes/admin/setting/profile.php";
